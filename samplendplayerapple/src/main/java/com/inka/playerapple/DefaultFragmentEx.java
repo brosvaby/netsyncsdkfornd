0d3cf88c97fe9d71011971ddc928226d4b256b47
oh seonggyun <birdgang82@gmail.com>
2018-05-31 17:08:12 +0900

    remove samplendplayerapple

--- samplendplayerapple/src/main/java/com/inka/playerapple/DefaultFragmentEx.java
+++
@@ -1,9 +0,0 @@
-package com.inka.playerapple;
-
-import com.inka.netsync.ui.fragment.DefaultFragment;
-
-/**
- * Created by birdgang on 2018. 1. 25..
- */
-public class DefaultFragmentEx extends DefaultFragment {
-}
